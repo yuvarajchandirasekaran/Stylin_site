0bc8acb0b280fdb891af26a3d075e49914d0884a
yuvarajchandirasekaran <yuvarajcdckap@gmail.com>
2024-03-27 16:06:18 +0530

    carousel and modal pop completed

--- index.js
+++ index.js
@@ -27,6 +27,70 @@ $(document).ready(function() {
     }
   })
 
+  // model-pop-up
+  $(document).ready(function() {
+    if ($(window).width() > 1000) {
+    $('.modal-pop-up').css('display', 'block');
+    document.body.style.overflow = 'hidden';
+
+    $('.form-close-btn').click(function(e) {
+        $('.modal-pop-up').css('display', 'none');
+        document.body.style.overflow = 'auto';
+    });
+  }
+});
+
+// carousel-1
+$(document).ready(function(){
+  $('.carousel-wrapper').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    draggable:false,
+    autoplaySpeed: 2000,
+    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slick-next">Next</button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
+// featured products tab view carousel
 
 
-  
+
+
+
+
+
+
+
